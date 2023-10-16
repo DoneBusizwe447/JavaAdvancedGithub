718350a3e7a7e126fead0c8e75f8936d8faf1c65
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-16 16:51:06 +0200

    yeah

---
+++ IGBooks/lesson2/DoWhileTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook1.lesson2;
+
+public class DoWhileTest {
+
+    public static void main(String args[]) {
+
+        int x = 30;
+
+        do {
+            System.out.print("value of x : " + x);
+            x++;
+            System.out.print("\n");
+        } while (x < 20);
+    }
+}

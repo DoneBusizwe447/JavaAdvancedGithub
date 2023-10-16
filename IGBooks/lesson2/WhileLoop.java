80306c91fabdfdd89e2f673ebac01148ddf3b354
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-16 16:49:02 +0200

    yeah

---
+++ IGBooks/lesson2/WhileLoop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook1.lesson2;
+public class WhileLoop {
+    
+    public static void main(String args[]){
+        
+        int i = 0;
+        int[] numbers = {100, 200, 300};
+        
+        while (i < numbers.length ){
+            System.out.println("Number: " + numbers[i]);
+            i++;
+        }
+        
+    }
+    
+}

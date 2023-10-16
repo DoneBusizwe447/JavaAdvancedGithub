d6c8e140d0ba241209650571283c2c19d5d58830
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-16 16:58:24 +0200

    casper

---
+++ Practices/Practice2/com/Employee.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practices.Practice2.com;
+
+public class Employee {
+
+    public int empId;
+    public String name;
+    public String ssn;
+    public double salary;
+
+    public Employee() {
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+}

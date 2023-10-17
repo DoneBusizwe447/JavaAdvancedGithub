99650543b2689f76ba425dffda6ac0f9ae147bb8
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-17 11:44:37 +0200

    gucci

---
+++ Practices/Practice3/com/example/domain/Director.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practices.Practice3.com.example.domain;
+
+public class Director extends Manager {
+
+    private double budget;
+
+    public Director(int empId, String name, String ssn, double salary, String department, double budget) {
+        super(empId, name, ssn, salary, department);
+        this.budget = budget;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+}

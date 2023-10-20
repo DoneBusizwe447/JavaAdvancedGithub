99f5c121fe3b1dc0117cc8bc00896c0423dcd83e
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-20 08:07:52 +0200

    lov

---
+++ IGBooks/lesson5/ElectronicDevice.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook1.lesson5;
+
+public abstract class ElectronicDevice {
+    
+    public abstract void turnOn();
+    
+    public abstract void turnOff();
+    
+}

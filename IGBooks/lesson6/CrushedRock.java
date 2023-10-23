379e754a688e2c457351d35fecd80308809d5b09
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-23 15:48:30 +0200

    tec

---
+++ IGBooks/lesson6/CrushedRock.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook1.lesson6;
+
+public class CrushedRock {
+  private String name;
+  private double salesPrice = 0;
+  private double cost = 0;
+  private double weight = 0; // In pounds
+  
+  public CrushedRock(double salesPrice, double cost, double weight){
+    this.salesPrice = salesPrice;
+    this.cost = cost;
+    this.weight = weight;
+  }  
+}

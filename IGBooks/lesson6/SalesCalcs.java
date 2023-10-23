3b0f9f82277517bb5fe320b294374d819ac64ec7
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-23 15:49:16 +0200

    tec

---
+++ IGBooks/lesson6/SalesCalcs.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook1.lesson6;
+
+public interface SalesCalcs {
+  public String getName();
+  public double calcSalesPrice();
+  public double calcCost();
+  public double calcProfit();
+}
+

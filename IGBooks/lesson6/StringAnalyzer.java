914aaa04488f8afc1bd6b3a2d799df5da6babd70
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-23 15:49:32 +0200

    tec

---
+++ IGBooks/lesson6/StringAnalyzer.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package igbook1.lesson6;
+
+
+public interface StringAnalyzer {
+  public boolean analyze(String sourceStr, String searchStr);
+}

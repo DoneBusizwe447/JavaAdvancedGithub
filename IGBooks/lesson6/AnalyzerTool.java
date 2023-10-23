2733bc03b7c146c70cf5cb9e4d2eb733d480e868
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-23 15:47:20 +0200

    tec

---
+++ IGBooks/lesson6/AnalyzerTool.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook1.lesson6;
+
+public class AnalyzerTool {
+  public boolean arrContains(String sourceStr, String searchStr){
+    return sourceStr.contains(searchStr);
+  }  
+}

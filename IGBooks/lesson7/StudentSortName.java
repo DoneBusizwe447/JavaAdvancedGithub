8d8e72b52aa584c1600c2155e4a9650feda5fe75
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-26 15:38:26 +0200

    hess

---
+++ IGBooks/lesson7/StudentSortName.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package igbook1.lesson7;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author magni
+ */
+public class StudentSortName implements Comparator<Student> {
+
+   
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        
+        
+        
+        return 0;
+        
+        
+        
+    }
+    
+}

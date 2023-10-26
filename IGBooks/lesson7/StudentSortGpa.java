7d0bf94893748cb46d883c61cbcf6102a84fa351
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-26 15:37:14 +0200

    hess

---
+++ IGBooks/lesson7/StudentSortGpa.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook1.lesson7;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author magni
+ */
+public class StudentSortGpa implements Comparator<Student> {
+
+    public StudentSortGpa() {
+    }
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        
+        return 0;
+        
+    }
+    
+}

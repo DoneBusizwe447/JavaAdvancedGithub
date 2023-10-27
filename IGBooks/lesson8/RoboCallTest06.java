7309e33551cda49d4b5843c2b0d6fd713b9eac1e
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-27 14:38:08 +0200

    moo

---
+++ IGBooks/lesson8/RoboCallTest06.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook1.lesson8;
+
+import java.util.List;
+
+public class RoboCallTest06 {
+  
+  public static void main(String[] args){ 
+
+    List<Person> pl = Person.createShortList();
+    
+    System.out.println("\n=== Print List ===");
+    pl.forEach(p -> System.out.println(p));
+        
+  }
+}

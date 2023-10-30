59f6975008c27295b91cd86c3795937809a1f202
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-10-30 15:53:03 +0200

    press

---
+++ IGBooks/lesson9/BuyerClass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook1.lesson9;
+
+public enum BuyerClass {
+    BASIC(0.0d),
+    SILVER(0.01d),
+    GOLD(0.02d),
+    PLATINUM(0.03d);
+    
+    private final double rate;
+    
+    BuyerClass(double rate){
+        this.rate = rate;
+    }
+    
+    public double getRate(){
+        return rate;
+    }
+    
+}

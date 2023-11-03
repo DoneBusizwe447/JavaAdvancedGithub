43586955703340094719991981a2345cdb04497a
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-11-03 17:19:02 +0200

    kasa

---
+++ IGBooks/lesson10/A02MapPeekTest.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook1.lesson10;
+
+import java.util.List;
+
+
+public class A02MapPeekTest {
+    
+    public static void main(String[] args) {
+
+        List<Employee> eList = Employee.createShortList();
+                
+        System.out.println("\n== CO Bonuses ==");
+        eList.stream()
+            .filter(e -> e.getRole().equals(Role.EXECUTIVE))
+            .filter(e -> e.getState().equals("CO"))
+            .peek(e -> System.out.print("Name: " 
+              + e.getGivenName() + " " + e.getSurName()))
+            .map(e -> e.getSalary() * Bonus.byRole(e.getRole()))
+            .forEach( s -> 
+              System.out.printf(
+                "  Bonus paid: $%,6.2f %n", s));
+        
+      
+        System.out.println("\n== Manager Bonus Report ==");
+        eList.stream()
+            .filter(e -> e.getRole().equals(Role.MANAGER))
+            .filter(e -> e.getDept().equals("Eng"))
+            .peek(e -> System.out.print("Name: " 
+              + e.getGivenName() + " " + e.getSurName()))
+            .map(e -> e.getSalary() * Bonus.byRole(e.getRole()))
+            .forEach( 
+                s -> System.out.printf(
+                  "  Bonus paid: $%,6.2f %n", s));
+    }  
+}

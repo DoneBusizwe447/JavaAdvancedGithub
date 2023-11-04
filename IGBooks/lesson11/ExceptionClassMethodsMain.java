8426a1d65ccea890854148610249555db608dce0
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-11-04 17:08:49 +0200

    chshoo

---
+++ IGBooks/lesson11/ExceptionClassMethodsMain.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook2.lesson11;
+
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class ExceptionClassMethodsMain {
+
+
+    public static void main(String[] args) {
+        
+        try {
+            System.out.println("About to open a file");
+            InputStream in = new FileInputStream("missingfile.txt");
+            System.out.println("File open");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }     
+    }
+}

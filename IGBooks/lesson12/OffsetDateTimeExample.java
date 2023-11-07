9fe729ae25a8a7d1d2ba96c5bb995e461a51e946
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-11-07 13:46:04 +0200

    chshoo

---
+++ IGBooks/lesson12/OffsetDateTimeExample.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook2.lesson12;
+
+
+import java.time.LocalDateTime;
+import static java.time.Month.*;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class OffsetDateTimeExample {
+
+  public static void main(String[] args) {
+    ZoneId SanFran = ZoneId.of("America/Los_Angeles");
+    ZoneId London = ZoneId.of("Europe/London");
+    LocalDateTime meeting = LocalDateTime.of(2014, JUNE, 13, 12, 30);
+    ZonedDateTime staffCall = ZonedDateTime.of(meeting, SanFran);
+    OffsetDateTime staffCallOffset = staffCall.toOffsetDateTime();
+    ZonedDateTime staffCallLondon = staffCallOffset.atZoneSameInstant(London);
+    //ZonedDateTime staffCallLondon = staffCall.withZoneSameInstant(London); 
+    System.out.println("Staff call (Pacific) is at: " + staffCall);
+    System.out.println("Staff call (UK) is at:      " + staffCallLondon);
+  }
+}

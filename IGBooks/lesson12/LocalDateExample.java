29333568aca71c31ebfcd60c59183b3e3ebc56db
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-11-08 16:33:18 +0200

    chshoo

---
+++ IGBooks/lesson12/LocalDateExample.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook2.lesson12;
+
+
+import java.time.LocalDate;
+import static java.time.temporal.TemporalAdjusters.*;
+import static java.time.DayOfWeek.*;
+import static java.lang.System.out;
+
+public class LocalDateExample {
+
+  public static void main(String[] args) {
+    LocalDate now, bDate, nowPlusMonth, nextTues;
+    now = LocalDate.now();
+    out.println("Now: " + now);
+    bDate = LocalDate.of(1995, 5, 23); // Java's Birthday
+    out.println("Java's Bday: " + bDate);
+    out.println("Is Java's Bday in the past? " + bDate.isBefore(now));
+    out.println("Is Java's Bday in a leap year? " + bDate.isLeapYear());
+    out.println("Java's Bday day of the week: " + bDate.getDayOfWeek());
+    nowPlusMonth = now.plusMonths(1);
+    System.out.println("Now: " + now);
+    out.println("The date a month from now: " + nowPlusMonth);
+    nextTues = now.with(next(TUESDAY));
+    out.println("Next Tuesday's date: " + nextTues);
+  }
+}

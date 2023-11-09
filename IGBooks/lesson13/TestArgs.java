3156239a7f0607a0db3a2115316911223f71004c
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-11-09 16:06:24 +0200

    chshoo

---
+++ IGBooks/lesson13/TestArgs.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook2.lesson13;
+
+
+public class TestArgs {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "] is ’" + args[i] + "’");
+        }
+    }
+}

bcdcaaa147fb4b945801275dbf4ed41ac801fb57
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-11-09 16:04:52 +0200

    chshoo

---
+++ IGBooks/lesson13/TestProperties.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook2.lesson13;
+
+
+import java.util.Properties;
+
+public class TestProperties {
+
+    public static void main(String[] args) {
+        Properties props = System.getProperties();
+        props.list(System.out);
+    }
+}

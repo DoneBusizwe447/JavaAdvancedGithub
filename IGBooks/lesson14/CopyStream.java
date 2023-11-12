d1d94590edfd79675221a6c418bebe1ec618527c
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-11-12 14:19:58 +0200

    chshoo

---
+++ IGBooks/lesson14/CopyStream.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbook2.lesson14;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardCopyOption.*;
+
+public class CopyStream {
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Usage: java CopyStream <URL> <file>");
+            System.err.println("CopyStream copies a web page to a file.");
+            System.exit(-1);
+        }
+
+        // Open the file to write to
+        Path path = Paths.get(args[1]);
+        URI u = URI.create(args[0]);
+        try (InputStream in = u.toURL().openStream()) {
+            Files.copy(in, path, REPLACE_EXISTING);
+        } catch (IOException e) {
+            System.err.println("Exception: " + e);
+        }
+    }
+}
